933afee476ad281ee27670500d11938723589dee
MarkLWorkman <70355546+MarkLWorkman@users.noreply.github.com>
2021-05-14 21:26:24 -0400

    server complete

--- api/server.js
+++ api/server.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
+const { logger, errorHandler } = require("./middleware/middleware");
+const projectsRouter = require("./projects/projects-router");
+const actionsRouter = require("./actions/actions-router");
+const helmet = require("helmet");
+
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use(express.json());
+server.use(helmet());
+server.use(logger);
+
+server.use("/api/projects", projectsRouter);
+server.use("/api/actions", actionsRouter);
+
+server.use("/", (req, res) => {
+  res.send("<h1>Lambda Web-Sprint-Challenge-Build-A-Web-Api</h1>");
+});
+
+server.use(errorHandler);
 
 module.exports = server;
